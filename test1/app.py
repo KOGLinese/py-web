9b647b8d013686259627920c31e22044101c1c8b
KOGLinese <1076452761@qq.com>
2018-07-01 17:01:16 +0800

    修改search的小bug

--- test1/app.py
+++ test1/app.py
@@ -31,14 +31,14 @@ def index():
 def login():
         if request.method == 'GET':
             return render_template('login.html')
-        else:
-            account = request.form.get('account')
-            password = request.form.get('password')
-            user = User.query.filter(User.password == password, User.account == account).first()
+        else:#点击登录按钮进入post请求
+            account = request.form.get('account')#从web上获得账号
+            password = request.form.get('password')#从web上获得密码
+            user = User.query.filter(User.password == password, User.account == account).first()#账号密码比较，得到用户信息
             if user:
-                session['user_id'] = user.id
+                session['user_id'] = user.id #将用户信息记入session中
                 session.permanent = True
-                return redirect(url_for('index', user=user))
+                return redirect(url_for('index', user=user))#传回主页
             else:
                 return u'账号或者密码错误'
 
@@ -47,22 +47,23 @@ def login():
 def regist():
     if request.method == 'GET':
         return render_template('regist.html')
-    else:
+    else:#post请求
+        #从前端的到数据
         account = request.form.get('account')
         username = request.form.get('name')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        user = User.query.filter(User.account == account).first()
+        user = User.query.filter(User.account == account).first()#用户重复筛选
         if user:
             return u'账号已被注册'
         else:
             if password1 != password2:
                 return u'两次密码不同'
             else:
-                user = User(account=account, username=username, password=password1)
-                db.session.add(user)
-                db.session.commit()
-                return redirect(url_for('login'))
+                user = User(account=account, username=username, password=password1)#创建数据
+                db.session.add(user)#插入到user表中
+                db.session.commit()#事物提交
+                return redirect(url_for('login'))#回到登录界面
 
 @app.route('/logout/')
 def logout():
@@ -81,14 +82,15 @@ def addmoney():
     if request.method == 'GET':
         return render_template('login.html')
     else:
+        #从前端获得充值金额
         addmon = request.form.get('money')
         addmon = float(addmon)
         password = request.form.get('password')
         user = User.query.filter(User.id == session['user_id']).first()
         if user:
             if password == user.password and addmon > 0:
-                user.money=user.money+addmon
-                db.session.commit()
+                user.money=user.money+addmon#修改数据
+                db.session.commit()#事物提交
                 return render_template('user.html', user=user)
             else:
                 return u'密码错误或者充值有误'
@@ -99,39 +101,40 @@ def addmoney():
 def search():
     if request.method == 'GET':
         return render_template('login.html')
-    else:
+    else:#post请求
+        #得到商品名称
         goodsname = request.form.get('goodsname')
+        #从4个表中进行查找
+        flag=0
         book = Books.query.filter(Books.bookname.like(goodsname)).all()
+        if book:
+            flag += 1
         clothe = Clothes.query.filter(Clothes.clothesname.like(goodsname)).all()
+        if clothe:
+            flag += 1
         digital = Digital.query.filter(Digital.digitalname.like(goodsname)).all()
+        if digital:
+            flag += 1
         eat = Eating.query.filter(Eating.eatingname.like(goodsname)).all()
-
-        context = {
-            'books': book,
-            'clothes': clothe,
-            'digitals': digital,
-            'eats': eat
-        }
-        if context is None:
-            flag = 1
-        else:
-            flag = 0
+        if eat:
+            flag += 1
+        context = {'books': book, 'clothes': clothe, 'digitals': digital, 'eats': eat}
         if session.get('user_id'):
             user = User.query.filter(User.id == session['user_id']).first()
-            return render_template('search.html', **context, user=user, flag=flag)
+            return render_template('search.html', **context, user=user, flag=flag)#进入search页面
         else:
             return redirect(url_for('login'))
 
-@app.route('/books/<id>')
+@app.route('/books/<id>')#传入参数id
 def judge1(id):
-    if session.get('user_id'):
+    if session.get('user_id'):#判断用户是否登录
         user = User.query.filter(User.id == session['user_id']).first()
-        good = Books.query.filter(Books.id == id).first()
+        good = Books.query.filter(Books.id == id).first()#从数据库中查询商品
         if good and user:
-            if user.money > good.bookprice and good.book_num > 0:
-                user.money = user.money-good.bookprice
-                good.book_num = good.book_num-1
-                db.session.commit()
+            if user.money >= good.bookprice and good.book_num > 0:#数量以及价格余额判断
+                user.money = user.money-good.bookprice#更新余额
+                good.book_num = good.book_num-1#更新数量
+                db.session.commit()#提交事务
                 return u'购买成功'
             elif user.money < good.bookprice:
                 return u'您的余额不足'
@@ -146,7 +149,7 @@ def judge2(id):
         user = User.query.filter(User.id == session['user_id']).first()
         good = Clothes.query.filter(Clothes.id == id).first()
         if good and user:
-            if user.money > good.clotheprice and good.clothes_num > 0:
+            if user.money >= good.clotheprice and good.clothes_num > 0:
                 user.money = user.money-good.clotheprice
                 good.clothes_num = good.clothes_num-1
                 db.session.commit()
@@ -164,7 +167,7 @@ def judge3(id):
         user = User.query.filter(User.id == session['user_id']).first()
         good = Digital.query.filter(Digital.id == id).first()
         if good and user:
-            if user.money > good.digitalprice and good.digital_num > 0:
+            if user.money >= good.digitalprice and good.digital_num > 0:
                 user.money = user.money-good.digitalprice
                 good.clothes_num = good.digital_num-1
                 db.session.commit()
@@ -182,7 +185,7 @@ def judge4(id):
         user = User.query.filter(User.id == session['user_id']).first()
         good = Eating.query.filter(Eating.id == id).first()
         if good and user:
-            if user.money > good.eatingprice and good.eating_num > 0:
+            if user.money >= good.eatingprice and good.eating_num > 0:
                 user.money = user.money-good.eatingprice
                 good.eating_num = good.eating_num-1
                 db.session.commit()
@@ -197,11 +200,11 @@ def judge4(id):
 @app.route('/books/')
 def books():
     context={
-        'books': Books.query.all()
+        'books': Books.query.all()#从数据库中查找所有数据
     }
     if session.get('user_id'):
         user = User.query.filter(User.id == session['user_id']).first()
-        return render_template('book.html', **context, user=user)
+        return render_template('book.html', **context, user=user)##将表单传入book界面
     else:
         return redirect(url_for('login'))
 
@@ -246,6 +249,6 @@ def my_context_prcessor():
     return {}
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
 #host='0.0.0.0',
